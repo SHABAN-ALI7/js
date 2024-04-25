badf0828a9ab8e42fe71a3c66c9515225fb60878
SHABAN ALI SHAIKH <108225719+SHABAN-ALI7@users.noreply.github.com>
2024-04-25 17:42:36 +0000

    datatypes

---
+++ 01/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" // treat all JS code as newwer version
+
+//alert(3+3) // we are using nodejs not browser
+
+/*
+=== PRIMITIVE DATATYPES ===
+
+number => 2^53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined => 
+symbol => uniqueness specially in React
+
+
+//object
+*/
+
+console.log(typeof("shaban"));
+console.log(typeof(undefined));  // undefined
+console.log(typeof(null))          // object or may be some language error
+
+console.table(["shaban" , null, undefined]);
